7bfdfa67826c23b1ce27f722e6bd746dc64c8bdf
Ximo Llacer <xllaber7@gmail.com>
2023-06-09 09:11:23 +0200

    Initial commit

---
+++ src/main/java/com/llacerximo/mybooks/MyBooksApplication.java
@@ -0,0 +1,13 @@
+package com.llacerximo.mybooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBooksApplication.class, args);
+    }
+
+}
