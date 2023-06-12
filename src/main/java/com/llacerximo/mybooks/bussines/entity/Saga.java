1327e0a2774d18722cafa01e7902d6f7d92c3dda
Ximo Llacer <xllaber7@gmail.com>
2023-06-12 09:16:25 +0200

    Create entities

---
+++ src/main/java/com/llacerximo/mybooks/bussines/entity/Saga.java
@@ -0,0 +1,28 @@
+package com.llacerximo.mybooks.bussines.entity;
+
+public class Saga {
+
+    private String id;
+    private String name;
+
+    public Saga(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
