3751fc8c498fa7f8dc83a6f8ebd456d6a5c7d0b4
Ximo Llacer <xllaber7@gmail.com>
2023-06-20 12:24:33 +0200

    Remove unnecessary annotation

--- src/main/java/com/llacerximo/mybooks/bussines/service/BookService.java
+++ src/main/java/com/llacerximo/mybooks/bussines/service/BookService.java
@@ -1,11 +1,10 @@
 package com.llacerximo.mybooks.bussines.service;
 
 import com.llacerximo.mybooks.bussines.entity.Book;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+
 public interface BookService {
     List<Book> getAll(int year);
 }
--- src/main/java/com/llacerximo/mybooks/bussines/service/impl/BookServiceImpl.java
+++ src/main/java/com/llacerximo/mybooks/bussines/service/impl/BookServiceImpl.java
@@ -3,14 +3,13 @@ package com.llacerximo.mybooks.bussines.service.impl;
 import com.llacerximo.mybooks.bussines.entity.Book;
 import com.llacerximo.mybooks.bussines.service.BookService;
 import com.llacerximo.mybooks.persistance.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.llacerximo.mybooks.persistance.impl.JdbcBookRepositoryImpl;
 
 import java.util.List;
 
 public class BookServiceImpl implements BookService {
 
-    @Autowired
-    private BookRepository bookRepository;
+    private BookRepository bookRepository = new JdbcBookRepositoryImpl();
 
     @Override
     public List<Book> getAll(int year) {
