fa77e0c479f938cc91337f8f8997e5fca504e672
Ximo Llacer <xllaber7@gmail.com>
2023-06-20 12:25:06 +0200

    Make main and getAll controllers

--- src/main/java/com/llacerximo/mybooks/controller/BookController.java
+++ src/main/java/com/llacerximo/mybooks/controller/BookController.java
@@ -1,10 +1,11 @@
 package com.llacerximo.mybooks.controller;
 
 import com.llacerximo.mybooks.bussines.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.llacerximo.mybooks.bussines.service.impl.BookServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,11 +14,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/books")
 public class BookController {
 
-    @Autowired
-    private BookService bookService;
+    private BookService bookService = new BookServiceImpl();
 
     @GetMapping("/{year}")
-    public String getAll(@RequestParam(defaultValue = "2023") int year, Model model) {
+    public String getAll(@PathVariable int year, Model model) {
         model.addAttribute("books", bookService.getAll(year));
         return "books";
     }
--- src/main/java/com/llacerximo/mybooks/controller/MainController.java
+++ src/main/java/com/llacerximo/mybooks/controller/MainController.java
@@ -1,13 +1,19 @@
 package com.llacerximo.mybooks.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String index() {
-        return "books";
+    public String index(Model model) {
+        Calendar calendar = new GregorianCalendar();
+        model.addAttribute("year", calendar.get(Calendar.YEAR));
+        return "index";
     }
 
 }
